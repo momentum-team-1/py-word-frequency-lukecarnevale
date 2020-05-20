487f475e13eca043dc748324bf6ad59532ec3389
lukecarnevale <lukecarnevale19@gmail.com>
2020-05-19 22:44:10 -0400

    cant connect to txt docs

--- word_frequency.py
+++ word_frequency.py
@@ -4,6 +4,35 @@ STOP_WORDS = [
     'will', 'with'
 ]
 
+Text = 'seneca_falls.txt'
+
+for char in '-.,/n':
+    Text=Text.replace(char,' ')
+Text = Text.lower
+
+print(Text)
+
+word_list = Text.split()
+print(word_list)
+
+d = {}
+
+for word in word_list:
+    d[word] = d.get(word, 0) + 1
+
+# for word in word_list:
+#     if word not in d:
+#         d[word] = 0
+#     d[word] += 1
+
+word_freq = []
+for key, value in d.items():
+    word_freq.append((value,key))
+word_freq
+
+word_freq.sort(reverse = True)
+print(word_freq)
+
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
